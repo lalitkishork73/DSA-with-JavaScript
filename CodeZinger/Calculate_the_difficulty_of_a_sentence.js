5329bfe88da53362c9e522b8b5e9583c0bfafdb8
chetang12 <102719631+chetang12@users.noreply.github.com>
2022-08-19 12:52:08 +0530

    codeZinger

---
+++ CodeZinger/Calculate_the_difficulty_of_a_sentence.js
@@ -0,0 +1,45 @@
+//Calculate the difficulty of a sentence
+
+// Given a string S, find the difficulty of the given string. Here a Word is considered hard if it has 4 consecutive consonants or number of consonants is more than the number of vowels. Else word is easy. Difficulty of the string is defined as 5*(number of hard words) + 3*(number of easy words).
+
+// Input:Difficulty of sentence
+
+// Output:13
+
+// Explanation:
+
+// Hard words = 2(Difficulty and sentence) Easy words = 1(of) So, answer is 5*2+3*1 = 13
+
+function solution (str){
+    str=str.split('')
+    let vowels={"a":1,"e":1,"i":1,"o":1,"u":1}
+    let vowel=0;
+    let consonant=0;
+    let conConsonant=0;
+    let hard=0;
+    let easy=0;
+    for(let i=0;i<str.length;i++){
+        if(!vowels[str[i].toLowerCase()] && str[i] != " "){
+            consonant++;
+            conConsonant++;
+        }else if(vowels[str[i].toLowerCase()]){
+            vowel++;
+            conConsonant=0;
+        }
+        if(str[i]==" "|| i== str.length -1){
+            if(conConsonant==4 ||consonant>vowel){
+                hard++;
+                consonant=0;
+                vowel=0;
+                conConsonant=0;
+            }else{
+                easy++;
+                consonant=0;
+                vowel=0;
+                conConsonant=0;
+            }
+        }
+    }
+    return 5*hard+3*easy
+}
+console.log(solution("Difficulty of sentence"))
---
+++ CodeZinger/Missing characters to make a string Pangram.js
@@ -0,0 +1,33 @@
+//Missing characters to make a string Pangram ?
+
+// Given a string S, find all characters that are missing from the string S, i.e., the characters that can make the string a Pangram. Display output in alphabetic order.
+
+// Input: The quick brown fox jumps
+// Output:adglvyz
+
+function solution (str){
+  str= str.toLowerCase().split("");
+  let arr=[];
+  for(let i=0;i<str.length;i++){
+    if(str[i]!=" "){
+      arr.push(str[i].charCodeAt(0)-"a".charCodeAt(0))
+    }
+  }
+  // Sorting and removing the duplicate
+  let map={}
+  for(let el of arr){
+    map[el] = (map[el] || 0 )+1
+  }
+  let arr1=[];
+  for(let i=0;i<26;i++){
+    if(!map[i]){
+      arr1.push(i)
+    }
+  }
+  let res="";
+  for(let num of arr1){
+    res += String.fromCharCode(num + "a".charCodeAt(0))
+  }
+  return res
+}
+console.log(solution("The quick brown fox jumps")) 
