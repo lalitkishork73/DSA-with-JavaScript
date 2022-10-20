f1ea60aa56c69b7af5e15f3c977824fe1f74c9c5
Lalitkishor Kanojiya <106534721+lalitkishork73@users.noreply.github.com>
2022-10-20 20:42:15 +0530

    done

--- Two-pointer/3sum.js
+++ Two-pointer/3sum.js
@@ -4,9 +4,11 @@ function ThreeSum(arr) {
   arr.sort((a, b) => a - b);
   let Trep = [];
   for (let i = 0; i < arr.length - 2; i++) {
+    // pass same number in array for x counter
     if (arr[i] === arr[i - 1]) {
       continue;
     }
+    // x+y+z=0 => y+z=-x => arr[l]+arr[r]=-arr[i]  // x=-4  y+z=4 =>0  logic
     let target = -arr[i];
     let l = i + 1,
       r = arr.length - 1;
