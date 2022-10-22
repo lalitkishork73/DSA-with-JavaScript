1c8a346aee4e1bb277d6e3c46f184e27bfc9c4fb
Lalitkishor Kanojiya <106534721+lalitkishork73@users.noreply.github.com>
2022-10-22 16:26:31 +0530

    add

--- practice/test.js
+++ practice/test.js
@@ -1,37 +1,23 @@
-//merge sorts
+let arr = [1, 2, 3, 9, 1, 4, 5, 8, 3, 5, 1],
+  k = 3;
 
-function mergeSort(arr) {
-  if (arr.length <= 1) {
-    return arr;
+let i = 0,
+  sum = 0,
+  maxSum = 0,
+  j = arr.length;
+
+while (i < k) {
+  sum += arr[i];
+  i++;
+}
+i = k;
+
+while (i < j) {
+  sum += arr[i] - arr[i - k];
+  if (sum > maxSum) {
+    maxSum = sum;
   }
-
-  let mid = Math.floor(arr.length / 2),
-    leftA = arr.slice(0, mid),
-    rightA = arr.slice(mid);
-
-  return merge(mergeSort(leftA), mergeSort(rightA));
+  i++;
 }
 
-function merge(leftA, rightA) {
-  let i = 0,
-    j = 0,
-    res = [];
-  let l = leftA.length,
-    r = rightA.length;
-  while (i < l || j < r) {
-    if (i === l) {
-      res.push(rightA[j]);
-      j++;
-    } else if (j === r || leftA[i] <= rightA[j]) {
-      res.push(leftA[i]);
-      i++;
-    } else {
-      res.push(rightA[j]);
-      j++;
-    }
-  }
-  return res;
-}
-
-let arr = [1, 22, 4, 51, 66, 28, 44, 14, 23, 56, 79];
-console.log(mergeSort(arr));
+console.log(maxSum);
