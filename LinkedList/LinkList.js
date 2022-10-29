94f693c857e538a682dceae897166b813c78a7f3
Lalitkishor Kanojiya <106534721+lalitkishork73@users.noreply.github.com>
2022-10-29 21:08:13 +0530

    added

--- LinkedList/LinkList.js
+++ LinkedList/LinkList.js
@@ -84,6 +84,25 @@
            this.head.next=null;
            this.head=first
     }
+    detectLoop(h) {
+        var s = new Set();
+        while (h != null) {
+            // If we have already has this node
+            // in hashmap it means their is a cycle
+            // (Because you we encountering the
+            // node second time).
+            if (s.has(h))
+                return true;
+ 
+            // If we are seeing the node for
+            // the first time, insert it in hash
+            s.add(h);
+ 
+            h = h.next;
+        }
+ 
+        return false;
+    }   
 }
 
    
