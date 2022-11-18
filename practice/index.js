333741df8d44dfd235a141f45cc00c2befaaafd8
Lalitkishor Kanojiya <106534721+lalitkishork73@users.noreply.github.com>
2022-11-18 11:13:29 +0530

    added

--- practice/index.js
+++ practice/index.js
@@ -1,6 +1,28 @@
 const inc=document.getElementById('inc');
 const dec=document.getElementById('dec');
+const tb=document.getElementById('root');
 const counter=document.getElementById('counter');
 let count=0
 
 
+async function getData(){
+    try{
+        return await fetch("https://api.publicapis.org/entries").then((res)=>console.log(res.json())).then(data=>console.log(data)).catch(err=>console.log(err));
+    }
+    catch(err){
+        console.log(err)
+    }
+}
+
+tb.innerHTML+="";
+
+
+getData();
+
+{/* <tr>
+                  <td>text1.1</td>
+                  <td>text1.2</td>
+                  <td>text1.2</td>
+                  <td>text1.2</td>
+                  <td>text1.3</td>
+                </tr> */}
