b37c647016deec468502ac84ab67c80e2f2a709a
Lalitkishor Kanojiya <106534721+lalitkishork73@users.noreply.github.com>
2022-11-25 13:08:20 +0530

    added test MERN

---
+++ MERN/src/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const {default:mongoose}= require('mongoose');
+const bodyParser = require('body-parser');
+const route = require('./routes/route');
+const app=express();
+const port=3000;
+
+app.use(bodyParser.json());
+
+mongoose.connect( "mongodb+srv://lalitkishork73:UzPr9bb6Wvxda9eC@cluster0.o2wavxe.mongodb.net/group27Database?retryWrites=true&w=majority",
+{
+  useNewUrlParser: true,
+})
+
+app.get('/test',()=>{
+  console.log('test');
+})
+
+app.listen(process.env.PORT || port, () => console.log(`connected to Port ${port}`));
