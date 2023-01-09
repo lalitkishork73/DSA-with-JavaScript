2547a3b435797fbeea9147bfda6345299cf88b4e
Lalitkishor Kanojiya <106534721+lalitkishork73@users.noreply.github.com>
2023-01-10 00:07:37 +0530

    added

--- NODE_JS/path.js
+++ NODE_JS/path.js
@@ -1,6 +1,17 @@
 
 const path = require('path');
 
-console.log(path.basename(__filename,'.js'),path.basename(__dirname)); 
+console.log(path.basename(__filename, '.js'), path.basename(__dirname));
 console.log(path.extname(__filename))
-console.log(path.join(__dirname,'lalit','/tamatar/jamana','out'))
+console.log(path.join(__dirname, 'lalit', '/tamatar/jamana', 'out'))
+console.log(path.join(__dirname, 'lalit', '/tamatar/jamana', '..'))
+console.log(path.join(__dirname, 'lalit', '/tamatar/jamana', '..', '..'))
+// console.log(path.normalize(''))
+console.log(path.parse(__filename))
+console.log(path.parse(__filename).name)
+console.log(path.isAbsolute('Array'))
+console.log(path.isAbsolute('/Array'))
+
+
+
+
