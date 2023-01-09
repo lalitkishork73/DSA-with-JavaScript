742ccba7cdfdc4206572e0622f80f96c724b5c73
Lalitkishor Kanojiya <106534721+lalitkishork73@users.noreply.github.com>
2023-01-09 23:57:18 +0530

    added

--- NODE_JS/path.js
+++ NODE_JS/path.js
@@ -1,4 +1,6 @@
-const { basename } = require('path');
+
 const path = require('path');
 
- 
+console.log(path.basename(__filename,'.js'),path.basename(__dirname)); 
+console.log(path.extname(__filename))
+console.log(path.join(__dirname,'lalit','/tamatar/jamana','out'))
