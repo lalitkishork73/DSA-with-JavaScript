59940ac9bf52d9a6fcc1929f9993d60d5ba6864d
Lalitkishor Kanojiya <106534721+lalitkishork73@users.noreply.github.com>
2023-04-02 23:45:22 +0530

    Update  MajorityElement.js

--- Array/ MajorityElement.js
+++ Array/ MajorityElement.js
@@ -14,7 +14,7 @@ n == nums.length
 1 <= n <= 5 * 104
 -109 <= nums[i] <= 109
 
-Follow-up: Could you solve the problem in linear time and in O(1) space? */
+Follow-up: Could you solve the problem in linear time and in O(1) space?  it will give you complex tasks*/
 
 switch (2) {
   case 1:
