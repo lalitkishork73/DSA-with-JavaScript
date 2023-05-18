07601875b926476b5a63ac4b873683d4531d74cc
Lalitkishor Kanojiya <106534721+lalitkishork73@users.noreply.github.com>
2023-05-18 06:25:17 +0000

    practice

--- practice/test3.js
+++ practice/test3.js
@@ -1,9 +1,36 @@
-let array=[1,2,5,6,9,53,21,2];
+// let array=[1,2,5,6,9,53,21,2];
 
-let b=array.filter((k)=>{
-    return k>2;
-});
+// let b=array.filter((k)=>{
+//     return k>2;
+// });
 
-let a=array.map(String)
-// console.log(b);
-console.log(a);
+// let a=array.map(String)
+// // console.log(b);
+// console.log(a);
+
+// function vector(data){
+//     let i=3000;
+//     setTimeout(()=>{
+//         data(i);
+//     },2000)
+// }
+
+
+// function mono(k){
+//     console.log('i love you ',k);
+// }
+
+// vector(mono);
+let n=5;
+let str=''
+for(let i=1;i<=n;i++){
+    for(let j=1;j<=i -1;j++){
+        str+=' ';
+    }
+    for(let k=i;k<=2*n-1;k++){
+        str+='*';
+    }
+    str+='\n'
+}
+
+console.log(str);
