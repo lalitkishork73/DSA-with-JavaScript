4293107a926ee1596c75f98369cdd72717fc89b6
Lalitkishor Kanojiya <106534721+lalitkishork73@users.noreply.github.com>
2023-05-20 06:56:51 +0000

    done

--- practice/test3.js
+++ practice/test3.js
@@ -36,24 +36,61 @@
 // }
 
 // console.log(map);
-let rows=5
-for(let i=1;i<=rows;i++){
-    let str=''
-    for(let j=1;j<=rows-i;j++){
-        str+=' '
+// let rows=5
+// for(let i=1;i<=rows;i++){
+//     let str=''
+//     for(let j=1;j<=rows-i;j++){
+//         str+=' '
+//     }
+//     for(let k=1;k<=i;k++){
+//         str+='* ';
+//     }
+//     console.log(str)
+// }
+// for(let i=rows-1;i>=1;i--){
+//     let str=''
+//     for(let j=1;j<=rows-i;j++){
+//         str+=' '
+//     }
+//     for(let k=1;k<=i;k++){
+//         str+='* ';
+//     }
+//     console.log(str)
+// }
+
+    
+let s='abbsadadef'
+
+const uniqueChars = [...new Set(s)];
+let maxLength = 0;
+
+  // Iterate through all possible pairs of characters
+  for (let i = 0; i < uniqueChars.length - 1; i++) {
+    for (let j = i + 1; j < uniqueChars.length; j++) {
+      const char1 = uniqueChars[i];
+      const char2 = uniqueChars[j];
+      let prevChar = '';
+      let count = 0;
+      let isValid = true;
+
+      // Iterate through the string and count the valid characters
+      for (let k = 0; k < s.length; k++) {
+        const currentChar = s[k];
+        if (currentChar === char1 || currentChar === char2) {
+          if (currentChar === prevChar) {
+            isValid = false;
+            break;
+          }
+          count++;
+          prevChar = currentChar;
+        }
+      }
+
+      // Update the maximum length if the current pair produces a longer valid string
+      if (isValid && count > maxLength) {
+        maxLength = count;
+      }
     }
-    for(let k=1;k<=i;k++){
-        str+='* ';
-    }
-    console.log(str)
-}
-for(let i=rows-1;i>=1;i--){
-    let str=''
-    for(let j=1;j<=rows-i;j++){
-        str+=' '
-    }
-    for(let k=1;k<=i;k++){
-        str+='* ';
-    }
-    console.log(str)
-}
+  }
+
+  console.log(maxLength);
